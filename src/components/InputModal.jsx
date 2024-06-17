63cab00226dee9f595c71bd6fbdc5c74b84898da
Himanshu Sharma <hs287190@gmail.com>
2024-06-17 16:02:35 +0530

    added select styling in input modal

--- src/components/InputModal.jsx
+++ src/components/InputModal.jsx
@@ -22,10 +22,10 @@ const InputModal = ({getModalData, handleModal}) => {
             <textarea className='bg-white h-full w-full outline-none p-2 font-mono' onKeyDown={handleEnter}></textarea>
         </div>
         <div className="w-1/6 flex flex-col items-center">
-            <button className="h-1/4 w-full bg-blue-200 hover:border-4 hover:border-white" onClick={() => setPriority(1)}></button>
-            <button className="h-1/4 w-full bg-blue-400 hover:border-4 hover:border-white" onClick={() => setPriority(2)}></button>
-            <button className="h-1/4 w-full bg-blue-600  hover:border-4 hover:border-white" onClick={() => setPriority(3)}></button>
-            <button className="h-1/4 w-full bg-blue-800  hover:border-4 hover:border-white" onClick={() => setPriority(4)}></button>
+            <button className={`h-1/4 w-full bg-blue-200 hover:border-4 hover:border-white ${(priority === 1) && 'border-8 border-white'} `} onClick={() => setPriority(1)}></button>
+            <button className={`h-1/4 w-full bg-blue-400 hover:border-4 hover:border-white  ${(priority === 2) && 'border-8 border-white'}`} onClick={() => setPriority(2)}></button>
+            <button className={`h-1/4 w-full bg-blue-600  hover:border-4 hover:border-white  ${(priority === 3) && 'border-8 border-white'}`} onClick={() => setPriority(3)}></button>
+            <button className={`h-1/4 w-full bg-blue-800  hover:border-4 hover:border-white  ${(priority === 4) && 'border-8 border-white'}`} onClick={() => setPriority(4)}></button>
         </div>
     </div>
   )
